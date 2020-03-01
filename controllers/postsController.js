d96ed87bd63bd8f9671c92266982988262c96a96
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-02-29 22:33:19 -0800

    database is now running successfully for posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,7 +3,12 @@ const db = require('../models');
 
 const index = (req, res) => {
 
-}
+db.Post.find({}, (err, allPosts) => {
+  if(err) return res.status(400).json({status:400, error: 'Something went wrong. Please try again.'});
+
+  res.json(allPosts)
+});
+};
 const show = (req, res) => {
 
 }
