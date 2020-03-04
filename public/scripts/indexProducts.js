cbe7004cc462b6992824b1a46200ea11378ca2de
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-03-03 17:44:56 -0800

    cleaning out index.html

--- public/scripts/indexProducts.js
+++ public/scripts/indexProducts.js
@@ -39,26 +39,3 @@ function getProductTemplate(product) {
 }
 
 
-
-/*
-  <div class="col-md-4 mb-4">
-  <div class="card">
-    <img src="${product.image}" class="card-img-top" alt="${
-product.name
-}" />
-    <div class="card-body">
-      <h5 class="card-title">${product.name}</h5>
-      <p class="card-text">
-        ${product.description}
-        (${product.posts.length} ${
-product.posts.length === 1 ? "post" : "posts"
-})
-      </p>
-      <a href="/cities/${
-        product._id
-      }" class="btn btn-primary float-right">View Details</a>
-    </div>
-  </div>
-</div>
-`;
-*/
