295d5e52d4840ed049e462be8c33aabced3db0a4
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-03-05 19:43:09 -0800

    pagination and product string length

--- public/scripts/indexProducts.js
+++ public/scripts/indexProducts.js
@@ -19,17 +19,40 @@ function render(productsArray) {
     productElement[1].insertAdjacentHTML("beforeend", productTemplates);
 }
 
+//render by type 
+// similar to below 
+
+
+
+// let productLinks = document.querySelectorAll('product_type');
+// let productLinksArr = 
+// addEventListener('click', filterProductsByType)
+// function filterProductsByType(event) {
+//   //grab all products in array
+//   let filterArray = document.getElementsByClassName('productContainer');
+//     //then filter array
+//   filterArray.filter(productContainer => {
+//     if (productContainer.getAttribute(productType) !== event.target.textContent){
+//       productContainer.display = "hidden"
+//     }
+//   })
+// }
+//
+
+
+
+
 function getProductTemplate(product) {
   return `
-  <div class="col-lg-4 col-md-6 mb-4">
+  <div productType=${product.product_type} class="productContainer col-lg-4 col-md-6 mb-4">
   <div class="card h-100">
     <a href="#"><img class="card-img-top" src=${product.image_link} alt=""></a>
     <div class="card-body">
       <h4 class="card-title">
-        <a href="#">${product.name}</a>
+        <a style="font-size: 16px;" href="/products/${product._id}">${product.name}</a>
       </h4>
   <h5>$ ${product.price}</h5>
-      <p class="card-text"> ${product.description}</p>
+      <p class="card-text"> ${product.description.substring(0,120)}...</p>
       <a href="/products/${product._id}" class="btn btn-primary float-right">More...</a>
     </div>
     <div class="card-footer">
